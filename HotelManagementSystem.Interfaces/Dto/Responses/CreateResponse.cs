1c65892e77e7142b83637a6db6d7826f9af78dc1
Vytautas Jaras <vjaras202@gmail.com>
2024-10-16 23:19:50 +0300

    clean-up

--- HotelManagementSystem.Interfaces/Dto/Responses/CreateResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HotelManagementSystem.Interfaces.Dto.Responses
-{
-    public class CreateResponse<T>
-    {
-        public Error? Error { get; set; }
-
-        public T? Model { get; set; }
-    }
-}
--- HotelManagementSystem.Interfaces/Dto/Responses/UpdateResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HotelManagementSystem.Interfaces.Dto.Responses
-{
-    public class UpdateResponse<T>
-    {
-        public Error? Error { get; set; }
-
-        public T? Model { get; set; }
-    }
-}
